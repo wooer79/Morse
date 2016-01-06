b1b8444bb465f558dbc7dfa753f37413305e8f53
wooer79 <ruanjianwei79@163.com>
2016-01-06 15:43:30 +0800

    FIRST commit

---
+++ Morse/src/org/morse/constant/Constant.java
@@ -0,0 +1,113 @@
+package org.morse.constant;
+
+public interface Constant {
+	public static final String A = ".-";
+	public static final String B = "-...";
+	public static final String C = "-.-.";
+	public static final String D = "-..";
+	public static final String E = ".";
+	public static final String F = "..-.";
+	public static final String G = "--.";
+	public static final String H = "....";
+	public static final String I = "..";
+	public static final String J = ".---";
+	public static final String K = "-.-";
+	public static final String L = ".-..";
+	public static final String M = "--";
+	public static final String N = "-.";
+	public static final String O = "---";
+	public static final String P = ".--.";
+	public static final String Q = "--.-";
+	public static final String R = ".-.";
+	public static final String S = "...";
+	public static final String T = "-";
+	public static final String U = "..-";
+	public static final String V = "...-";
+	public static final String W = ".--";
+	public static final String X = "-..-";
+	public static final String Y = "-.--";
+	public static final String Z = "--..";
+	public static final String NUM_1 = ".----";
+	public static final String NUM_2 = "..---";
+	public static final String NUM_3 = "...--";
+	public static final String NUM_4 = "....-";
+	public static final String NUM_5 = ".....";
+	public static final String NUM_6 = "-....";
+	public static final String NUM_7 = "--...";
+	public static final String NUM_8 = "---..";
+	public static final String NUM_9 = "----.";
+	public static final String NUM_0 = "-----";
+	/**
+	 * 句号
+	 */
+	public static final String PERIOD = ".-.-.-";
+	/**
+	 * 冒号
+	 */
+	public static final String COLON = "---...";
+	/**
+	 * 逗号
+	 */
+	public static final String COMMA = "--..--";
+	/**
+	 * 分号
+	 */
+	public static final String SEMICOLON = "-.-.-.";
+	/**
+	 * 问号
+	 */
+	public static final String QUESTION_MASK = "..--..";
+	/**
+	 * 等号
+	 */
+	public static final String EQUAL_SIGN = "-...-";
+	/**
+	 * 单引号
+	 */
+	public static final String SINGLE_QUOTE = ".----.";
+	/**
+	 * 斜杠
+	 */
+	public static final String OBLIQUE = "-..-.";
+	/**
+	 * 感叹号
+	 */
+	public static final String EXCLAMATION_POINT = "-.-.--";
+	/**
+	 * 破折号
+	 */
+	public static final String DASH = "-....-";
+	/**
+	 * 底线
+	 */
+	public static final String UNDERSCORE = "..--.-";
+	/**
+	 * 双引号
+	 */
+	public static final String QUOTE = ".-..-.";
+	/**
+	 * 前括号
+	 */
+	public static final String OPEN_PAREN = "-.--.";
+	/**
+	 * 后括号
+	 */
+	public static final String CLOSE_PAREN = "-.--.-";
+	/**
+	 * $
+	 */
+	public static final String DOLLAR_SIGN = "...-..-";
+	/**
+	 * @
+	 */
+	public static final String AT_SIGN = ".--.-.";
+	/**
+	 * &
+	 */
+	public static final String AND = "....";
+	/**
+	 * +
+	 */
+	public static final String PLUS = ".-.-.";
+	
+}
